c571fc2637876d9a8f480ba784f537270c177a88
basrulyandri <digicrea08@gmail.com>
2018-11-05 00:02:57 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.backend.master')
+
+@section('content')
+    <div class="note note-info">
+        <p> A black page template with a minimal dependency assets to use as a base for any custom page you create </p>
+    </div>
+@stop
